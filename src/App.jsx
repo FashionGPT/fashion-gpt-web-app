cb709a3bdaf3d73b8a82777cab57b873ee763fde
Joo Lim <103789061+jlim0128@users.noreply.github.com>
2023-10-14 15:54:43 -0400

    Linking auth0 with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const [count, setCount] = useState(0)
-
-    function Redirect() {
-        window.location.assign("http://localhost:5173/api/auth/login");
-    }
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
 
     return (
     <>
       <h1>Fashion-GPT</h1>
       <div className="card">
-        <button onClick={() => Redirect()}>
-          Login
-        </button>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+          Log Out</button>
       </div>
       <p className="read-the-docs">
         Click on the Login for Auth0
