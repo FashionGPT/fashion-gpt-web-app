b6256bb0f84c0c09ac5b062d7fec7b748e6402c0
lakimapturn <venomizer11@gmail.com>
2023-10-23 00:59:16 -0400

    added fashiongpt component

---
+++ src/components/Demo/FashionGPT.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { baseAPIURL } from "../../constants/constants";
+
+const FashionGPT = () => {
+  const [userInput, setUserInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertData, setAlertData] = useState({});
+
+  const fetchChatGPTResponse = async () => {
+    setIsLoading(true);
+
+    const data = { prompt: userInput };
+
+    try {
+      const response = await fetch(
+        `${baseAPIURL}/ChatGPT/create-outfit-from-chatgpt`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await response.json();
+      alert(
+        `Pants: ${result.pants}\nShirt: ${result.shirt}\nShoes: ${result.shoes}`
+      );
+      setShowAlert(true);
+      setAlertData(result);
+    } catch (error) {
+      console.log(`FashionGPT Error: ${error}`);
+    }
+
+    setIsLoading(false);
+  };
+
+  const onTextAreaChangeHandler = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div>
+      <textarea value={userInput} onChange={onTextAreaChangeHandler}></textarea>
+      <button type="button" onClick={fetchChatGPTResponse}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default FashionGPT;
---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
+import Nav from "./components/Navbar/Nav";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>
+            <Auth0ProviderWithHistory>
+                <Nav />
+                <App />
+            </Auth0ProviderWithHistory>
+        </div>,
+    },
+    {
+        path: "/sign-in",
+        element: <div>TODO: sign in page</div>,
+    },
+    {
+        path: "/sign-up",
+        element: <div>TODO: sign up page</div>,
+    },
+    {
+        path: "/prompt",
+        element: <div>TODO: prompt page</div>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router}>
+            <Auth0ProviderWithHistory>
+                <Nav />
+                <App />
+            </Auth0ProviderWithHistory>
+        </RouterProvider>
+    </React.StrictMode>,
+)
