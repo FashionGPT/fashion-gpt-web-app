379050433fcf983877e4635c2a4b10d8dd787289
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-11-13 01:08:09 -0500

    Render clothes on community page

--- src/components/Community/communityPage.jsx
+++ src/components/Community/communityPage.jsx
@@ -6,80 +6,81 @@ import "./card.css";
 
 function CommunityPage () {
 
-    const [posts, setPosts] = React.useState([
-        {
-            _id: "6515c8057b142891511ab2f8",
-            title: "test title",
-            text: "test texts",
-            user: "user",
-            outfit: "test"
-        },
-        {
-            _id: "6515c8057b142891511ab2f8",
-            title: "test title",
-            text: "test texts",
-            user: "user",
-            outfit: "test"
-        }
-    ]);
+    const [posts, setPosts] = React.useState([]);
     const [titleText, setTitleText] = React.useState('');
     const [postText, setPostText] = React.useState('');
     const [outfitText, setOutfitText] = React.useState('');
 
     const [outfits, setOutfits] = React.useState([]);
-    
-    React.useEffect(() => {
-      
+    const [selectedOutfitId, setSelectedOutfitId] = React.useState('');
+    const [userId, setUserId] = React.useState("6513978b5a313f06321da9eb");
+
+    function fetchPosts() {
         let config = {
-          method: 'get',
-          maxBodyLength: Infinity,
-          url: 'http://localhost:8081/api/v1/Post/community',
-          headers: { }
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:8081/api/v1/Post/community',
+            headers: { }
         };
-        
+
         axios.request(config)
+            .then((response) => {
+                let arr = response?.data?.result;
+                if (arr) {
+                    arr = arr.reverse();
+                }
+                setPosts(arr);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    React.useEffect(() => {
+
+        fetchPosts();
+
+        axios.post('http://localhost:8081/api/v1/Clothing/outfits-for-user', {
+            "userID": userId
+        })
         .then((response) => {
-            setPosts(response.data.result);
+            let arr = response?.data?.result;
+            if (arr) {
+                arr = arr.reverse();
+            }
+            console.debug("Got outfits for user", arr);
+            setOutfits(arr);
+            if (arr && arr.length > 0) {
+                setSelectedOutfitId(arr[0]._id);
+            }
         })
         .catch((error) => {
-          console.log(error);
+            console.error("Error getting outfits for user", error);
         });
 
-        let config2 = {
-          method: 'post',
-          url: 'http://localhost:8081/api/v1/Clothing/outfits-for-user',
-          body: {
-            "userID": "651397a75a313f06321da9ec"
-            },
-          headers: { }
-        };
-        
-        axios.request(config2)
-        .then((response) => {
-            setOutfits(response.data.result);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-        
     }, []);
-    
+
     return (
         <div>
             <h1>Community Page</h1>
             <input placeholder='title' value={titleText} onChange={(event) => {setTitleText(event.target.value)}}></input>
             <input placeholder='text' value={postText} onChange={(event) => {setPostText(event.target.value)}}></input>
-            <select id="outfitForm" placeholder="select outfit" data-style="btn-info" name="selectpicker">
+            <select id="outfitForm" placeholder="select outfit" data-style="btn-info" name="selectpicker" onChange={(e) => {
+                setSelectedOutfitId(e?.target?.value || '');
+            }}>
                 {
                   outfits.map((outfit) => {
                     return (
                     <option
                       key={outfit._id}
+                      value={outfit._id}
                       onClick={() => {
-                        setOutfitText(outfit.shirt.name + ", " + outfit.pants.name + ", " + outfit.shoes.name);
+                        setOutfitText(outfit?.shirt?.name + ", " + outfit?.pants?.name + ", " + outfit?.shoes?.name);
+                        // setSelectedOutfitId(outfit?._id || '');
+                        console.debug("Selected outfit", outfit?._id);
                       }}
                       >
-                        {outfit.shirt.name + ", " + outfit.pants.name + ", " + outfit.shoes.name}
+                        {outfit?.shirt?.name + ", " + outfit?.pants?.name + ", " + outfit?.shoes?.name}
                       </option>);
                   })
                 }
@@ -88,41 +89,71 @@ function CommunityPage () {
                 let data = JSON.stringify({
                     "title": titleText,
                     "text": postText,
-                    "outfit": outfitText,//"65139624709491f3b5286cf4",
-                    "user": "6513978b5a313f06321da9eb"
+                    "outfit": selectedOutfitId,//"65139624709491f3b5286cf4",
+                    "user": userId
                   });
-                  
+
                   let config = {
                     method: 'post',
                     maxBodyLength: Infinity,
                     url: 'http://localhost:8081/api/v1/Post/create',
-                    headers: { 
+                    headers: {
                       'Content-Type': 'application/json'
                     },
                     data : data
                   };
-                  
+
                   axios.request(config)
                   .then((response) => {
                     console.log(JSON.stringify(response.data));
+                    setTitleText('');
+                    setPostText('');
+                    fetchPosts();
                   })
                   .catch((error) => {
                     console.log(error);
                   });
-                
+
             }}>Submit</button>
             {
                 posts.map((post) => {
                     return (
-                        <div className="card-container">
+                        <div className="card-container" key={post._id}>
                             <p>{post.title}</p>
                             <p>{post.text}</p>
-                            <p>{post.outfit}</p>
+                            <div>
+                                { post.outfit.shirt &&
+                                    <div>
+                                        <p>
+                                            {post.outfit.shirt.name}
+                                        </p>
+                                        <img src={post.outfit.shirt.imageUrl}></img>
+                                    </div>
+                                }
+
+                                { post.outfit.pants &&
+                                    <div>
+                                        <p>
+                                            {post.outfit.pants.name}
+                                        </p>
+                                        <img src={post.outfit.pants.imageUrl}></img>
+                                    </div>
+                                }
+
+                                { post.outfit.shoes &&
+                                    <div>
+                                        <p>
+                                            {post.outfit.shoes.name}
+                                        </p>
+                                        <img src={post.outfit.shoes.imageUrl}></img>
+                                    </div>
+                                }
+                            </div>
                         </div>
                     )
                 })
             }
-        </div>  
+        </div>
     )
 }
 
