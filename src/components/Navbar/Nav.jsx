46f5f0711b943463d1cb4bb881654236df21f2e6
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-11-14 14:49:49 -0500

    link auth and fix clothing generation

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,9 +1,14 @@
 import "./Nav.css";
 import {useAuth0} from "@auth0/auth0-react";
+import {useEffect} from "react";
 
 function Nav() {
   const { logout, loginWithRedirect } = useAuth0();
   const { user } = useAuth0();
+
+    useEffect(() => {
+        console.log(user);
+    }, [user]);
 
   return (
       <div className="navbar">
